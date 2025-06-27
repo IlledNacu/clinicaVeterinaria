0a1170a0e3e7cad4dc961207a7f8d93ffdb5ea3c
Illed <illed.nacucchio@gmail.com>
2025-06-27 18:01:49 -0300

    Usando artisan

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,9 +1,11 @@
-<?php //Crearlo con Laravel y luego hacer esto
+<?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Admin extends Controller {
+class AdminController extends Controller
+{
     public function index() {
         return view('admin.index');
     }
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Rutas para el administrador
-Route::get('/admin', [App\Http\Controllers\Admin::class, 'index'])->name('admin.index')->middleware('auth');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
 // La autenticación de middleware lo que hace es que no se puede acceder a la ruta a menos que se esté logueado, y si te logueás te redirige automáticamente a donde querías entrar
