4db2cf6326e19d5e8d0b874a405e86187c30bec8
IlledNacu <bkilled7@gmail.com>
2025-06-27 15:58:57 -0300

    Se traen los usuarios desde el Controlador en la función index que retorna la vista usuarios.index, y se le pasa la variable con la información a listar

--- app/Http/Controllers/Usuario.php
+++ app/Http/Controllers/Usuario.php
@@ -1,10 +1,12 @@
 <?php //Crearlo con Laravel y luego hacer esto; con el nombre correspondiente y cambiarlo en las rutas
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class Usuario extends Controller {
     public function index() {
-        return view('admin.usuarios.index');
+        $usuarios = User::all();
+        return view('admin.usuarios.index', compact('usuarios'));
     }
 }
