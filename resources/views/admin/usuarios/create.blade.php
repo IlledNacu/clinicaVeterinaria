47b411494143456b3909b5618680e2049eb37637
IlledNacu <bkilled7@gmail.com>
2025-07-07 16:40:29 -0300

    Validaciones de laravel con @error y recuperación de los datos validados con value={{old()}}

--- resources/views/admin/usuarios/create.blade.php
+++ resources/views/admin/usuarios/create.blade.php
@@ -13,19 +13,31 @@
             @csrf <!-- campo token -->
             <div class="mb-3 form-group">
                 <label for="name" class="form-label">Nombre</label><b> *</b>
-                <input type="text" class="form-control" id="name" name="name" required>
+                <input type="text" value="{{old('name')}}" class="form-control" id="name" name="name" required>
+                @error('name')
+                <small style="color:red">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3 form-group">
                 <label for="email" class="form-label">Email</label><b> *</b>
-                <input type="email" class="form-control" id="email" name="email" required>
+                <input type="email" value="{{old('email')}}" class="form-control" id="email" name="email" required>
+                @error('email')
+                <small style="color:red">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3 form-group">
                 <label for="password" class="form-label">Contraseña</label><b> *</b>
                 <input type="password" class="form-control" id="password" name="password" required>
+                @error('password')
+                <small style="color:red">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3 form-group">
                 <label for="password_verify" class="form-label">Repetir contraseña</label><b> *</b>
                 <input type="password" class="form-control" id="password_verify" name="password_confirmation" required>
+                @error('password_confirmation')
+                <small style="color:red">{{$message}}</small>
+                @enderror
             </div>
             <div class="mb-3 form-group">
                 <button type="submit" class="btn btn-primary">Guardar</button>
