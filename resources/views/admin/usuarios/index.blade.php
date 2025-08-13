8aecec9ad90c6f564f7f4c4d123d9a123ddf368a
IlledNacu <bkilled7@gmail.com>
2025-08-13 14:08:54 -0300

    Traducción de las tablas -19- al español - 20

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -43,18 +43,58 @@
         <script>
           $(function () {
             $("#example1").DataTable({
-              "responsive": true, "lengthChange": false, "autoWidth": false,
-              "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+              "pageLength": 10,
+              "language": {
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Usuarios",
+                "infoFiltered": "(Filtrado de _MAX_ total Usuarios",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Usuarios",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscador:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                  "first": "Primero",
+                  "last": "Último",
+                  "next": "Siguiente",
+                  "previous": "Anterior"
+                }
+              },
+              "responsive": true, "lengthChange": true, "autoWidth": false,
+              "buttons": [{
+                  extend: 'collection',
+                  text: 'Reportes',
+                  orientation: 'landscape',
+                  buttons: [{
+                    text: 'Copiar',
+                    extend: 'copy'
+                  }, {
+                    extend: 'pdf'
+                  }, {
+                    extend: 'csv'
+                  }, {
+                    extend: 'excel'
+                  }, {
+                    text: 'Imprimir',
+                    extend: 'print'
+                  }]
+              }, {
+                extend: 'colvis',
+                text: 'Visor de columnas',
+                collectionLayout: 'fixed three-column'
+              }]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-            $('#example2').DataTable({
-              "paging": true,
-              "lengthChange": false,
-              "searching": false,
-              "ordering": true,
-              "info": true,
-              "autoWidth": false,
-              "responsive": true,
-            });
+            // $('#example2').DataTable({
+            //   "paging": true,
+            //   "lengthChange": false,
+            //   "searching": false,
+            //   "ordering": true,
+            //   "info": true,
+            //   "autoWidth": false,
+            //   "responsive": true,
+            // });
           });
         </script>
       </div><!-- /.container-fluid -->
