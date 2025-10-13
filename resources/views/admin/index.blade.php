d4da5b147a350784b1c9ca4a194974d6ecf04a3e
IlledNacu <bkilled7@gmail.com>
2025-10-13 16:05:50 -0300

    Cantidad de usuarios - DASH.26/Tabla de secretarias.27

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,11 +7,73 @@
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-
+    <hr>
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <p>Acá va el contenido principal</p>
+        
+      <div class="row">
+        <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+            <h3>{{$total_usuarios}}</h3>
+
+            <p>Usuarios</p>
+            </div>
+            <div class="icon">
+            <i class="ion fas bi bi-file-person"></i>
+            </div>
+        </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+            <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+            <p>Bounce Rate</p>
+            </div>
+            <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+            <h3>44</h3>
+
+            <p>User Registrations</p>
+            </div>
+            <div class="icon">
+            <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+            <h3>65</h3>
+
+            <p>Unique Visitors</p>
+            </div>
+            <div class="icon">
+            <i class="ion ion-pie-graph"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        </div>
+        <!-- ./col -->
+      </div>
+
       </div><!-- /.container-fluid -->
     </div>
   <!-- /.content -->
